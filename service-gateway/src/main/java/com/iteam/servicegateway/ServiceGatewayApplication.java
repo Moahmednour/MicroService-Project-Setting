469d081725f3a26d2b9f30fb710a87d32f68c2b5
Moahmednour <mednourbensalah@gmail.com>
2024-04-22 09:04:10 +0100

    add static route for .net

--- service-gateway/src/main/java/com/iteam/servicegateway/ServiceGatewayApplication.java
+++ service-gateway/src/main/java/com/iteam/servicegateway/ServiceGatewayApplication.java
@@ -1,7 +1,10 @@
 package com.iteam.servicegateway;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
@@ -9,21 +12,26 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceGatewayApplication {
-	// @Bean
-	// RouteLocator routes(RouteLocatorBuilder builder) {
-	// 	return builder.routes().route(r -> r.path("/societes/**").uri("http://localhost:8081")).build();
-	// }
+	
+	@Bean
+	RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder.routes().route(r -> r.path("/api/product/**").uri("http://localhost:8082")).build();
+	}
+
 	// @Bean
 	// RouteLocator routes(RouteLocatorBuilder builder) {
 	// return builder.routes().route(r ->
-	// r.path("/societes/**").uri("lb://SERVICE-SOCIETE")).build();
+	// r.path("/api/product/**").uri("lb://PRODUCT-SERVICE")).build();
 	// }
 
 	@Bean
-	DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp) {
+	public DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc,
+			DiscoveryLocatorProperties dlp) {
 		return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
 	}
 
